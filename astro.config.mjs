6c733fea40e4be0e8c735bedc1d0e20b5e5ee4f9
starjardin <tantely.and@onja.org>
2024-01-04 16:09:56 +0300

    [feature]: adds vercel addapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://starjardin.github.io',
+  output: "server",
+  adapter: vercel()
 });
