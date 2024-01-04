ccd7d86e0f7d9783f4d3c7cdb212fa313ac2be2f
starjardin <tantely.and@onja.org>
2024-01-04 15:43:14 +0300

    fix: correct page url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astronaut.github.io',
+  site: 'https://starjardin.github.io',
   base: '/my-repo',
 });
